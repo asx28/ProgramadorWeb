31ed9e14514e80192da527479bedecea1331bf00
Professora Amanda <119063182+asx28@users.noreply.github.com>
2023-01-24 18:57:58 -0300

    Add files via upload

--- formulario.js
+++ formulario.js
@@ -3,41 +3,59 @@ botaoAdicionar.addEventListener("click", enviar);
 
 function enviar(event){
     event.preventDefault();
-    console.log("Cadastro solicitado");
+    //console.log("Cadastro solicitado");
     var form = document.querySelector("#form");
     
     //1-Pegar os inputs do formulário:
-    var nome = form.nome.value;
-    var peso = form.peso.value;
-    var altura = form.altura.value;
-    var gordura = form.gordura.value;
+    var paciente = obterPacienteDoForm(form);
+    var pacienteTr = montaTr(paciente);
+    console.log(pacienteTr)
 
+    //6 - Anexar a linha <tr> na tabela <tbory> existente
+    var tabela = document.querySelector("#tabela-pacientes");
+    tabela.appendChild(pacienteTr);
+
+    form.reset();
+}
+
+//Refatorando o Create do CRUD
+function obterPacienteDoForm(form){
+    var paciente = {
+        nome: form.nome.value,
+        peso: form.peso.value,
+        altura: form.altura.value,
+        gordura: form.gordura.value,
+        imc: calculaImc(form.peso.value,form.altura.value)
+    }
+    return paciente;
+}
+
+function montaTr(paciente){
     //2 - Criar a linha da tabela <tr>
     var pacienteTr = document.createElement("tr");
+    pacienteTr.classList.add("paciente");
     
     //3 - Criar coluna da tabela <td>
-    var nomeTd = document.createElement("td");
-    var pesoTd = document.createElement("td");
-    var alturaTd = document.createElement("td");
-    var gorduraTd = document.createElement("td");
+    var nomeTd = montaTd(paciente.nome, "info-nome"); 
+    var pesoTd = montaTd(paciente.peso, "info-peso");
+    var alturaTd = montaTd(paciente.altura, "info-altura");
+    var gorduraTd = montaTd(paciente.gordura, "info-gordura");
+    var imcTd = montaTd(paciente.imc, "info-imc");
    
-
-    //4 - Preencher as células <td> com os inputs do formulário
-    nomeTd.textContent = nome;
-    pesoTd.textContent = peso;
-    alturaTd.textContent = altura;
-    gorduraTd.textContent = gordura;
-
     //5 - Anexar as colunas <td> na linha <tr> da tabela
     pacienteTr.appendChild(nomeTd);
     pacienteTr.appendChild(pesoTd);
     pacienteTr.appendChild(alturaTd);
     pacienteTr.appendChild(gorduraTd);
+    pacienteTr.appendChild(imcTd);
 
-    console.log(pacienteTr);
-
-    //6 - Anexar a linha <tr> na tabela <tbory> existente
-    var tabela = document.querySelector("#tabela-pacientes");
-    tabela.appendChild(pacienteTr);
+    return pacienteTr;
 }
 
+function montaTd(dado, classe){
+    var td = document.createElement("td");
+    td.textContent = dado;
+    td.classList.add(classe);
+
+    return td;
+}
